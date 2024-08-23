c2c2acb63624159591b4d1b6f7be73c41a1cd76e
Hironori Ichimiya <hiron@hironytic.com>
2024-08-23 16:58:20 +0900

    Add distination

--- iosdc2024ibeacon/ViewController.swift
+++ iosdc2024ibeacon/ViewController.swift
@@ -19,27 +19,47 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
 
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
         if manager.authorizationStatus == .authorizedWhenInUse {
+            label.text = "start monitoring"
             manager.startMonitoring(for: beaconRegion)
         } else {
+            label.text = "stop monitoring"
             manager.stopMonitoring(for: beaconRegion)
         }
     }
 
     func locationManager(_ manager: CLLocationManager, didDetermineState state: CLRegionState, for region: CLRegion) {
+        guard let beaconRegion = region as? CLBeaconRegion else { return }
         switch state {
         case .inside: // 領域内
             label.text = "inside"
+            manager.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
         case .outside: // 領域外
             label.text = "outside"
+            manager.stopRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
         case .unknown: // 不明
             label.text = "unknown"
+            manager.stopRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        switch beacons.first!.proximity {
+        case .far:
+            label.text = "inside far"
+        case .immediate:
+            label.text = "inside immediate"
+        case .near:
+            label.text = "inside near"
+        case .unknown:
+            label.text = "inside unknown"
+        @unknown default: return
         }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        label.text = "initializing"
+        label.text = ""
         locationManager = CLLocationManager()
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
